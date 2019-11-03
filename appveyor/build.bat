77ebe7767ccb0358c787ba185ab6e65ab691acde
Christoph M. Becker <cmbecker69@gmx.de>
2019-11-03 23:48:14 +0100

    WTF?

--- appveyor/build.bat
+++ appveyor/build.bat
@@ -1,3 +1,1 @@
-C:\projects\php-meminfo\php-7.3.11-devel-VC15-x64\phpize
-configure --enable-meminfo
-nmake
+C:\projects\php-meminfo\php-7.3.11-devel-VC15-x64\phpize && configure --enable-meminfo && nmake
